bb4b1186826d9b6d181b0d92c25bb3af839a3bd6
ngn-anh <143248926+NDQuynh25@users.noreply.github.com>
2026-03-08 16:46:16 +0700

    chore: update default API URL to production endpoint

--- fe-dashboard/src/apis/ApiService.ts
+++ fe-dashboard/src/apis/ApiService.ts
@@ -11,7 +11,7 @@ import { useLoadingStore } from '../store/useLoadingStore';
  */
 const CONFIG = {
   ssl: false, // Set to true if using HTTPS
-  api_url: import.meta.env.API_URL || 'localhost:5210/api/v1', 
+  api_url: import.meta.env.API_URL || 'https://demo-dotnet.onrender.com/api/v1', 
 };
 
 const HTTP_METHODS: Method[] = ['get', 'post', 'put', 'delete', 'patch'];
