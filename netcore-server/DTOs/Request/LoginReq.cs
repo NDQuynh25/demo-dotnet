d8139c5c1618d31f540ce2da616dfeaddb8d628d
ngn-anh <143248926+NDQuynh25@users.noreply.github.com>
2026-03-08 14:06:22 +0700

    feat: add data validation attributes to request DTOs

--- netcore-server/DTOs/Request/LoginReq.cs
+++ netcore-server/DTOs/Request/LoginReq.cs
@@ -1,8 +1,11 @@
 ﻿namespace netcore_server.DTOs.Request;
-
+using System.ComponentModel.DataAnnotations;
 public class LoginReq
 {
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Email invalid")]
     public string Email { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = string.Empty;
 }
--- netcore-server/DTOs/Request/UserReq.cs
+++ netcore-server/DTOs/Request/UserReq.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace netcore_server.DTOs.Request;
 
 public class UserReq
 {
+    [Required(ErrorMessage = "Full name is required")]
     public string FullName { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Email invalid")]
     public string Email { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Date of birth is required")]
     public string DateOfBirth { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Phone number is required")]
+    [Phone(ErrorMessage = "Phone number invalid")]
     public string PhoneNumber { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Address is required")]
     public string Address { get; set; } = string.Empty;
 }
